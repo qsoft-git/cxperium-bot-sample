0247fa5308536d15fa9b9903e0c5ee88e8c15a65
Erdinç Cürebal <erdinccurebal@hotmail.com>
2023-12-26 14:40:13 +0300

    first init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import { app } from "../engine";
+// Envrionments.
+const { NODE_ENV } = process.env;
 
-app.listen(3000, () => {
+let nodeEnvWithImportEngine: string = '';
+
+switch (NODE_ENV) {
+  case 'development':
+    nodeEnvWithImportEngine = '../engine';
+    break;
+
+  case 'production':
+    nodeEnvWithImportEngine = 'cxperium-bot-engine';
+    break;
+
+  default:
+    throw new Error('NODE_ENV is not set.');
+}
+
+main();
+
+async function main() {
+  const { app } = await import(nodeEnvWithImportEngine);
+
+  app.listen(3000, () => {
     console.log('Listening on port 3000');
-});
+  });
+}
